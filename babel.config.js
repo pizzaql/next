69fcee98e3be32b65202d9bdedb67ea57214cf5b
Antoni Kepinski <keppi@o2.pl>
2018-09-13 20:16:04 +0300

    Use babel.config.js instead of .babelrc :unicorn:

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const presets = [
+  ["@babel/env", {
+    targets: {
+      edge: "17",
+      firefox: "62",
+      chrome: "69",
+      safari: "11.1",
+      opera: "55"
+    }]
+];
+
+module.exports = { presets };
+   
