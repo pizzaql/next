13a636dc6a9e41279bc363a92dfd4e732d2556f1
Antoni Kepinski <keppi@o2.pl>
2018-09-14 15:50:16 +0300

    Use the latest browser versions

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 const presets = [
   ["@babel/env", {
-    targets: {
-      edge: "17",
-      firefox: "62",
-      chrome: "69",
-      safari: "11.1",
-      opera: "55"
-    },
+    targets: "last 1 version",
     }],
 ];
 
