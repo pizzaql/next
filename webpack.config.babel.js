3ceb468b55651443e7a71856ec9b399b5f293241
Antoni Kepinski <keppi@o2.pl>
2018-09-17 00:25:50 +0300

    Making it a PWA 🚀

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,6 +3,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ResourceHintWebpackPlugin from 'resource-hints-webpack-plugin';
 import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
+import WebpackPwaManifest from 'webpack-pwa-manifest';
 import OfflinePlugin from 'offline-plugin';
 
 module.exports = {
@@ -70,6 +71,23 @@ module.exports = {
 		}),
 		new ScriptExtHtmlWebpackPlugin({
 			defaultAttribute: 'async'
+		}),
+		/* eslint-disable camelcase */
+		new WebpackPwaManifest({
+			name: 'kepinski.me',
+			short_name: 'kepinski',
+			description: 'Personal Website of Antoni Kepinski',
+			background_color: '#212121',
+			icons: [
+				{
+					src: path.resolve('src/images/rocket-192.png'),
+					size: '192x192'
+				},
+				{
+					src: path.resolve('src/images/rocket-512.png'),
+					size: '512x512'
+				}
+			]
 		}),
 		new OfflinePlugin()
 	]
