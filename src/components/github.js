7a322002c315c45cdb09d310e0e6aa0d779201d7
Antoni Kepinski <xxczaki@pm.me>
2019-04-10 18:28:45 +0200

    Add ripple effect

--- src/components/github.js
+++ src/components/github.js
@@ -5,16 +5,24 @@ const Github = styled.a`
   font-size: 14px;
   font-weight: 500;
   padding: 8px 10px;
+  border: none;
   border-radius: 3px;
-  transition: all .3s ease;
+  transition: background 0.8s;
   background-color: #333;
   background-position: center;
   text-decoration: none;
   user-select: none;
+  outline: none;
 
-  &:hover {
-      background-color: #444;
-  }
+    &:hover {
+        background: #444 radial-gradient(circle, transparent 1%, #444 1%) center/15000%;
+    }
+
+    &:active {
+        background-color: #555;
+        background-size: 100%;
+        transition: background 0s;
+    }
 `;
 
 export default Github;
