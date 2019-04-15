459db40af61e789e654c686bdfbaec8bacaa09b0
Antoni Kepinski <xxczaki@pm.me>
2019-04-15 19:44:57 +0200

    Add tags with technologies next to project name

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,13 +5,40 @@ import List from './list';
 import Link from './link';
 import Github from './github';
 import Vector from './vector';
+import Tag from './tag';
 import projects from './projects.json';
+
+const tag = tags => {
+	const getTagColor = name => {
+		if (name === 'React') {
+			return {background: '#61dbfb', color: '#000'};
+		}
+
+		if (name === 'Node.js') {
+			return {background: '#68a063', color: '#000'};
+		}
+
+		if (name === 'GraphQL') {
+			return {background: '#e535ab', color: '#000'};
+		}
+
+		if (name === 'Next.js') {
+			return {background: '#000', color: '#fff'};
+		}
+
+		if (name === 'Electron') {
+			return {background: '#2f3243', color: '#fff'};
+		}
+	};
+
+	return tags.map(tag => <Tag key={tag} style={{backgroundColor: getTagColor(tag).background, color: getTagColor(tag).color}}>{tag}</Tag>);
+};
 
 const Projects = () => (
 	<>
 		<List>
 			{projects.map(el => (
-				<li key={el.name}><Link href={el.url} target="_blank" rel="noopener">{el.name}</Link></li>
+				<li key={el.name}><Link href={el.url} target="_blank" rel="noopener">{el.name}</Link> {tag(el.tags)}</li>
 			))}
 		</List>
 		<Github href="https://github.com/xxczaki?tab=repositories" target="_blank" rel="noopener"><Vector draggable="false" importance="low" loading="lazy" src={withPrefix('/img/github.svg')} alt="Github"/>See more on GitHub</Github>
---
+++ src/components/tag.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Tag = styled.span`
+    display: inline-flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -webkit-box-align: center;
+    vertical-align: 2px;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
+    border: none;
+    border-radius: 2px;
+    box-shadow: none;
+    min-width: 20px;
+    max-width: 100%;
+    height: 15px;
+    padding: 2px 6px;
+    line-height: 14px;
+    font-size: 11px;
+    margin-bottom: 10px;
+    margin-left: 1px;
+    margin-right: 4px;
+`;
+
+export default Tag;
