4f8a440efa51fb3cc4261da7eea21456fa889c85
Antoni Kepinski <xxczaki@pm.me>
2019-11-06 22:40:22 +0100

    fix spacing between elements & add description

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Heading, Button, Stack, Text, Tag} from '@chakra-ui/core';
+import {Box, Heading, Button, Stack, Text, Tag, Link} from '@chakra-ui/core';
 
 import projects from './projects.json';
 import getTagColor from './utils/tag-color';
@@ -12,19 +12,23 @@ const Projects = () => {
 			<Text>Here are some of my projects:</Text>
 			<br/>
 			{projects.map(el => (
-				<Box key={el.name} p={5} rounded="lg" shadow="md">
-					<Heading paddingBottom={2} as="h3" size="md">{el.name}</Heading>
-					<Stack isInline spacing={2}>
-						{el.tags.map(tag => (
-							<Tag key={tag} style={{color: getTagColor(tag).color}} backgroundColor={getTagColor(tag).background} size="sm">{tag}</Tag>
-						))}
-					</Stack>
-					<a href={el.url} target="_blank" rel="noopener noreferrer">
-						<Button marginTop={5} leftIcon="external-link" variantColor="gray.600" variant="outline">
+				<React.Fragment key={el.name}>
+					<Box key={el.name} p={5} rounded="lg" shadow="md">
+						<Heading paddingBottom={2} as="h3" size="md">{el.name}</Heading>
+						<Stack isInline spacing={2}>
+							{el.tags.map(tag => (
+								<Tag key={tag} style={{color: getTagColor(tag).color}} backgroundColor={getTagColor(tag).background} size="sm">{tag}</Tag>
+							))}
+						</Stack>
+						<Text paddingTop={3}>{el.description}</Text>
+						<Link isExternal href={el.url}>
+							<Button marginTop={5} leftIcon="external-link" variantColor="gray.600" variant="outline">
     Check it out
-						</Button>
-					</a>
-				</Box>
+							</Button>
+						</Link>
+					</Box>
+					<br/>
+				</React.Fragment>
 			))}
 		</Box>
 	);
