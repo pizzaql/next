6b9af9dbcf7f95713d60847622c2545af49a05d4
Antoni Kepinski <xxczaki@pm.me>
2019-11-06 22:40:01 +0100

    Add source code link

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,9 @@ import {Text, Link} from '@chakra-ui/core';
 const Footer = () => (
 	<footer>
 		<Text>Copyright © 2019 <Link href="#" color="teal.500">Antoni Kepinski</Link></Text>
+		<Link isExternal href="https://github.com/xxczaki/kepinski.me" color="teal.500">
+			Source code
+		</Link>
 	</footer>
 );
 
