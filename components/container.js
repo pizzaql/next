a34a79231da39eb6802cbc31df983fd3eabac7be
Antoni Kepinski <xxczaki@pm.me>
2019-12-30 20:16:26 +0100

    Fix padding-right on very small screens

--- components/container.js
+++ components/container.js
@@ -11,11 +11,11 @@ const Wrapper = styled.div`
     flex-direction: column;
     flex-wrap: wrap;
     margin: 0 auto 30px;
-	padding-left: 5em;
+	padding-left: 3em;
+	padding-right: 20px;
 
 	@media (min-width: 320px) and (max-width: 480px) {
 		padding-left: 20px;
-		padding-right: 20px;
 	}
 `;
 
