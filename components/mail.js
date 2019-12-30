600770c13971189b858883b3645da2d4bcac2212
Antoni Kepinski <xxczaki@pm.me>
2019-12-30 13:32:42 +0100

    Change animation

--- components/mail.js
+++ components/mail.js
@@ -1,46 +1,23 @@
 import React from 'react';
-import styled, {keyframes} from 'styled-components';
-
-const shake = keyframes`
-	10%, 90% {
-		transform: translate3d(-1px, 0, 0);
-	}
-
-	20%, 80% {
-		transform: translate3d(2px, 0, 0);
-	}
-
-	30%, 50%, 70% {
-		transform: translate3d(-4px, 0, 0);
-	}
-
-	40%, 60% {
-		transform: translate3d(4px, 0, 0);
-	}
-`;
-
-const Animated = styled.div`
-	&:hover {
-		animation: ${shake} 0.82s cubic-bezier(.36,.07,.19,.97) both;
-  		transform: translate3d(0, 0, 0);
-  		backface-visibility: hidden;
-  		perspective: 1000px;
-	}
-`;
+import styled from 'styled-components';
 
 const Wrapper = styled.a`
 	text-align: center;
 	text-decoration: none;
 	color: #fff;
     font-size: 25px;
+	box-shadow: inset 0 -3px 0 #424242;
+	transition: all .2s ease-in-out;
+
+	&:hover {
+		box-shadow: inset 0 -33px 0 0 #424242;
+	}
 `;
 
 const Mail = () => (
-	<Animated>
-		<Wrapper href="mailto:a@kepinski.me">
+	<Wrapper href="mailto:a@kepinski.me">
 			a@kepinski.me
-		</Wrapper>
-	</Animated>
+	</Wrapper>
 );
 
 export default Mail;
