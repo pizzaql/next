b01cb7e43e2b9fbad56f77d9c85be660a93bf244
Antoni Kepinski <a@kepinski.me>
2020-07-16 15:54:50 +0200

    revert: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,12 @@ import About from '../components/about';
 import TechStack from '../components/tech-stack';
 import Contact from '../components/contact';
 
-const Index: NextPage<unknown> = () => {
-	console.log(process.env.VERCEL_GITHUB_COMMIT_SHA);
-	console.log(process.env.NOW_GITHUB_COMMIT_SHA);
-	return (
-		<Main>
-			<About/>
-			<TechStack/>
-			<Contact/>
-		</Main>
-	);
-};
+const Index: NextPage<unknown> = () => (
+	<Main>
+		<About/>
+		<TechStack/>
+		<Contact/>
+	</Main>
+);
 
 export default Index;
