b1ee5630a9f71c8e58ad58fc0e5dd608530a0a33
Antoni Kepinski <a@kepinski.me>
2020-09-08 21:03:37 +0200

    Make logo smaller

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,8 @@
+import {Flash} from '@styled-icons/ionicons-outline';
 import styled from 'styled-components';
 
-const Logo = styled.img`
-    width: 2.5em;
+const Logo = styled(Flash)`
+    width: 2.35em;
     cursor: pointer;
     transition: background-color var(--transition);
     display: inline-flex;
@@ -9,6 +10,7 @@ const Logo = styled.img`
 	margin: -10px;
 	border-radius: var(--inline-radius);
 	margin-right: var(--gap-half);
+	color: var(--text);
 
     &:hover {
         background-color: var(--hover);
