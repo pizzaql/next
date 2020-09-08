b267494124542ad4679f31744aaaa366701367b4
Antoni Kepinski <a@kepinski.me>
2020-09-08 21:02:24 +0200

    Clean up _app.tsx, add recoil for state management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,98 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {AppProps} from 'next/app';
 import Head from 'next/head';
 import Router from 'next/router';
-import {createGlobalStyle} from 'styled-components';
+import {RecoilRoot} from 'recoil';
 import debounce from 'lodash.debounce';
 import nprogress from 'nprogress';
 
 import Container from '../components/container';
-
-const GlobalStyle = createGlobalStyle`
-	:root {
-		--gap-half: 0.5rem;
-		--gap: 1rem;
-		--gap-double: 2rem;
-		--small-gap: 4rem;
-		--main-content: 55rem;
-		--radius: 8px;
-		--inline-radius: 5px;
-		--background: #131415;
-		--text: #fff;
-		--gray: #222;
-		--light-gray: #666;
-		--header: #131415cc;
-		--hover: #2b2d30;
-		--selection: hsl(0deg 0% 100% / 99%);
-		--vibrancy: saturate(180%) blur(20px);
-		--font-sans: 'Inter', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-		--transition: 0.1s ease-in-out;
-		--transition-slow: 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0s;
-	}
-
-	@font-face {
-		font-family: 'Inter';
-		font-style: normal;
-		font-weight: 400;
-		font-display: swap;
-		src: local(''),
-			url('/fonts/inter-v1-latin-regular.woff2') format('woff2'),
-			url('/fonts/inter-v1-latin-regular.woff') format('woff');
-	}
-
-	@font-face {
-		font-family: 'Inter';
-		font-style: normal;
-		font-weight: 800;
-		font-display: swap;
-		src: local(''),
-			url('../public/fonts/inter-v1-latin-800.woff2') format('woff2'),
-			url('../public/fonts/inter-v1-latin-800.woff') format('woff');
-	}
-
-	body {
-		align-items: center;
-		justify-content: center;
-		font-family: var(--font-sans);
-		background-color: var(--background);
-		color: var(--text);
-		font-size: 1.125rem;
-		margin-top: -1em;
-		-webkit-font-smoothing: antialiased;
-		text-rendering: optimizeSpeed;
-		overflow-x: hidden;
-	}
-
-	#nprogress {
-		pointer-events: none;
-	}
-
-	#nprogress .bar {
-		position: fixed;
-		z-index: 2000;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 5px;
-		background: #fafbfc;
-	}
-
-	#nprogress::after {
-		content: '';
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 5px;
-		background: transparent;
-	}
-
-	::selection {
-		text-shadow: none;
-		background: var(--selection);
-		color: var(--background);
-	}
-`;
+import {_theme} from '../lib/recoil-atoms';
 
 // Only show nprogress after 500ms (slow loading)
 const start = debounce(nprogress.start, 500);
@@ -107,16 +22,40 @@ Router.events.on('routeChangeError', () => {
 	nprogress.done();
 });
 
-const myApp = ({Component, pageProps}: Readonly<AppProps>): JSX.Element => (
-	<>
-		<GlobalStyle/>
-		<Head>
-			<title>Antoni Kepinski</title>
-		</Head>
-		<Container>
-			<Component {...pageProps}/>
-		</Container>
-	</>
-);
+const App = ({Component, pageProps}: Readonly<AppProps>): JSX.Element => {
+	const [theme, setTheme] = useState<'dark' | 'light' | undefined>(undefined);
 
-export default myApp;
+	useEffect(() => {
+		const previous = localStorage.getItem('theme');
+
+		if (previous) {
+			setTheme(previous as 'dark' | 'light');
+		} else {
+			localStorage.setItem('theme', 'dark');
+			setTheme('dark');
+		}
+	}, []);
+
+	return (
+		<>
+			<Head>
+				<title>Antoni Kepinski</title>
+			</Head>
+			{theme && (
+				<RecoilRoot
+					initializeState={({set}) => {
+						if (theme) {
+							set(_theme, theme);
+						}
+					}}
+				>
+					<Container>
+						<Component {...pageProps}/>
+					</Container>
+				</RecoilRoot>
+			)}
+		</>
+	);
+};
+
+export default App;
