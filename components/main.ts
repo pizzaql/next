be9ff56d40ca018669924b15b476dff304e91304
Antoni Kepinski <a@kepinski.me>
2020-09-08 21:03:48 +0200

    Improve overall page responsiveness

--- components/main.ts
+++ components/main.ts
@@ -6,8 +6,12 @@ const Main = styled.main`
 	margin: 0 auto;
 	line-height: 1.9;
 	max-width: var(--main-content);
-	padding: 0 1rem;
+	padding: 4rem 1.5rem 1.5rem;
 	align-items: flex-start;
+
+	@media (min-width: 150px) and (max-width: 891px) {
+		padding: 1.5rem;
+	}
 `;
 
 export default Main;
