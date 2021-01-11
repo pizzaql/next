02ebe2e2989b4f1bb186c33bb709a5dc370a9ada
Antoni Kepinski <xxczaki@pm.me>
2021-01-11 16:46:20 +0100

    Translate page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {NextPage} from 'next';
+import useTranslation from 'next-translate/useTranslation';
 
-const Index: NextPage<unknown> = () => (
-	<h1>This page cannot be found.</h1>
-);
+const Index: NextPage<unknown> = () => {
+	const {t} = useTranslation('404');
+
+	return (
+		<h1>{t('message')}</h1>
+	);
+};
 
 export default Index;
