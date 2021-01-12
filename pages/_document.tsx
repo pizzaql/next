1a4d622e10ab076d9dc8eba642d9065f1017e157
Antoni Kepinski <xxczaki@pm.me>
2021-01-12 03:36:05 +0100

    Detect language in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
 import {ColorModeScript} from '@chakra-ui/react';
 
 import info from '../lib/info';
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return {...initialProps};
-	}
-
 	render(): JSX.Element {
+		const language = this.props.__NEXT_DATA__.locale;
+
 		return (
 			<Html>
-				<Head>
+				<Head lang={language}>
 					<meta charSet="utf-8"/>
-					<meta name="description" content={info.description}/>
+					<meta name="description" content={info.description[language as 'en' | 'pl']}/>
 					<meta name="theme-color" content="#131415"/>
 					<meta name="msapplication-TileColor" content="#131415"/>
 					<meta name="apple-mobile-web-app-capable" content="yes"/>
@@ -23,12 +20,12 @@ export default class MyDocument extends Document {
 					<meta name="twitter:card" content="summary_large_image"/>
 					<meta name="twitter:site" content="https://pizzaql.vercel.app"/>
 					<meta name="twitter:title" content={info.name}/>
-					<meta name="twitter:description" content={info.description}/>
+					<meta name="twitter:description" content={info.description[language as 'en' | 'pl']}/>
 					<meta name="twitter:image" content=""/>
 					<meta property="og:title" content={info.name}/>
 					<meta property="og:site_name" content={info.name}/>
 					<meta property="og:url" content="https://pizzaql.vercel.app"/>
-					<meta property="og:description" content={info.description}/>
+					<meta property="og:description" content={info.description[language as 'en' | 'pl']}/>
 					<meta property="og:type" content="website"/>
 					<meta property="og:image" content=""/>
 					<link rel="manifest" href="/manifest.json"/>
